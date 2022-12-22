1108099f287c3843de9deb5ea59d6be8773f22f2
tks18 <tksudharshan@gmail.com>
2022-12-22 22:05:20 +0530

    fix(port): allow port to be configured in env

--- app.py
+++ app.py
@@ -5,8 +5,12 @@ load_dotenv(dotenv_path=path.join(path.dirname(__file__), ".env"))
 
 from app import Server
 from app import routes
+from app.plugins import get_env_variable
 
+port = get_env_variable("PORT")
 
-investment_server = Server("investment-server", "0.0.0.0", 5000, routes)
+investment_server = Server(
+    "investment-server", "0.0.0.0", port if port else 5000, routes
+)
 
 investment_server.start_server()
